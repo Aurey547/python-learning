677ba21e413c4360c13e1633fc970cec3072d64f
Aurey547 <darkaurorazara@gmail.com>
2026-06-19 22:30:57 +0100

    Create study_tracker.py

---
+++ study_tracker.py
@@ -0,0 +1,23 @@
+study_hours ={}
+print("Welcome Shobiye to your study tracker")
+while True:
+  print("\n === STUDY TRACKER ===")
+  print("1. Add Study Session")
+  print("2. View Progress")
+  print("3. Exit")
+
+choice = input("Choose an option: ")
+
+if choice == "1"
+ subject = input("Enter Subject")
+ hours = float(input("Hours Studied"))
+
+if subject in study_hours:
+   study_hours[subject] += hours
+else:
+  study_hours[subjects] = hours
+
+print(f"{hours} hours added to {subject}!")
+
+elif choice = = "2":
+ print("
