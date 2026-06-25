53970f9e0e4aadd32a77255e45f60cd4c40e6837
Temmy <darkaurorazara@gmail.com>
2026-06-25 19:38:03 +0100

    Add notification feature and improve UI colors

--- study_tracker.py
+++ study_tracker.py
@@ -1,38 +1,56 @@
-study_hours ={}
-print("Welcome Shobiye to your study tracker")
+from colorama import Fore, init
+init()
+
+from plyer import notification
+    
+def send_notification():
+    notification.notify(
+        title="Study Reminder",
+        message="Time to study! Future Cryptographer!💕",
+        timeout=10
+    )
+study_hours = {}
+goal = 5
+print(Fore.CYAN + "Welcome Shobiye to your study tracker")
+send_notification()
 while True:
-  print("\n === STUDY TRACKER ===")
-  print("1. Add Study Session")
-  print("2. View Progress")
-  print("3. Exit")
+    print(Fore.MAGENTA +"\n === STUDY TRACKER ===")
+    print("1. Add Study Session")
+    print("2. View Progress")
+    print("3. Exit")
 
-choice = input("Choose an option: ")
+    choice = input("Choose an option: ")
 
-if choice == "1"
- subject = input("Enter Subject")
- hours = float(input("Hours Studied"))
+    if choice == "1":
+        subject = input("Enter Subject: ")
+        hours = float(input("Hours Studied: "))
 
-if subject in study_hours:
-   study_hours[subject] += hours
-else:
-  study_hours[subject] = hours
+        if subject in study_hours:
+            study_hours[subject] += hours
+        else:
+            study_hours[subject] = hours
 
-print(f"{hours} hours added to {subject}!")
+        print(f"{hours} hours added to {subject}!")
 
-elif choice = = "2":
- print("\n=== YOUR PROGESS ===")
+    elif choice == "2":
+        print("\n=== YOUR PROGRESS ===")
 
-total_hours = 0
+        total_hours = 0
 
-for subject,hours in study_hours.items():
-  print(f{subject}:{hours} hours")
-  total_hours += hours
+        for subject, hours in study_hours.items():
+            print(f"{subject}: {hours} hours")
+            total_hours += hours
 
-print(f"\nTotal Study Time: {total_hours} hours")
+        print(f"\nTotal Study Time: {total_hours} hours")
 
-elif choice = = "3"
- print("Goodbye! Keep studying Temmy!)
-  break
+        if total_hours >= goal:
+            print(Fore.GREEN + f"Congratulations🤩! You have reached your goal of {goal} hours.")
+        else:
+            print(Fore.RED + f"You are {goal - total_hours} hours away from your goal😡.")
 
-else:
- print("Invalid option")
+    elif choice == "3":
+        print(Fore.GREEN + "Goodbye! Keep studying Temmy😁!")
+        break
+
+    else:
+        print("Invalid option")
